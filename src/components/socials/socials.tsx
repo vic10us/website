4de4c7d6b01d01dcd41c0cac9f61816ede80d747
vic10us <dev@vic10.us>
2022-07-01 22:50:12 +0000

    feat: :sparkles: adding link to instructables

--- src/components/socials/socials.tsx
+++ src/components/socials/socials.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import SocialButton from './social-button';
 import './socials.scss';
+import './icons/style.css';
 
 class SocialsView extends Component {
   render() {
@@ -10,6 +11,7 @@ class SocialsView extends Component {
         <SocialButton href="https://www.github.com/vic10us" icon="github"/>
         <SocialButton href="https://www.facebook.com/vic10usx" icon="facebook-f"/>
         <SocialButton href="https://www.youtube.com/vic10us" icon="youtube"/>
+        <SocialButton href="https://www.instructables.com/member/vic10us/" icon="instructables"/>
       </div>
     );
   }
